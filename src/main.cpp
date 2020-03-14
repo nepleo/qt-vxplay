59abf8e0ae5b42eae15528592dd325d152fa3181
Leo <41498518+nepleo@users.noreply.github.com>
2020-03-14 12:15:38 +0800

    delete comments

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,6 @@ int main(int argc, char *argv[])
 	QApplication a(argc, argv);
 	xplay w;
 	w.show();
-	//char* url = "rtmp://58.200.131.2:1935/livetv/hunantv";   //ºşÄÏTV
+	//char* url = "rtmp://58.200.131.2:1935/livetv/hunantv";   
 	return a.exec();
 }
